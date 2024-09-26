c64d3b431bd49c44561b2cfc21cacf883d5d1552
sahilchaudhary <sahil.chaudhary@tarento.com>
2024-09-26 18:46:43 +0530

    Adding impl for report download

--- src/main/java/org/sunbird/common/util/Constants.java
+++ src/main/java/org/sunbird/common/util/Constants.java
@@ -1209,6 +1209,7 @@ public class Constants {
 	public static final String PREVIOUS_TERM_CODE = "previousTermCode";
 	public static final String DESIGNATION_MASTER_DATA = "designation_master_data";
 	public static final String COMPETENCY_MASTER_DATA = "competency_master_data";
+	public static final String CHILD_ID = "childId";
 
 	private Constants() {
 		throw new IllegalStateException("Utility class");
